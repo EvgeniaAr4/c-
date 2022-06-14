e4f30e024bf26983ba2019e29b7ce02d5ecd5e2b
Evgenia <olivia20011978@mail.ru>
2022-06-14 17:31:40 +0300

    add

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿// 1. Напишите программу, которая на вход принимает число и выдаёт его квадрат
+
+// Console.WriteLine("input integer number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int result = num * num;
+// Console.WriteLine("result is " + result);
+
+//2. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое квадратом второго
+
+// Console.WriteLine("input first number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input second number");
+// // int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int square = num2 * num2;
+
+// if (num == square)
+//     Console.WriteLine("first number is a square of second number");
+
+// else
+//     Console.WriteLine("first number is not a square of second number");
+
+//3. напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+// Console.WriteLine("input number of a day: ");
+// int day = Convert.ToInt32(Console.ReadLine());
+
+// if (day < 1 || day > 7)
+//      Console.Write("incorrect day");
+
+// else
+
+//     if (day == 1)
+//         Console.WriteLine("monday");
+
+//4. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутки от -N до N.
+
+// Console.Write("input N");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int current;
+// if (num < 0)
+//    current = num;
+//    num = num * (-1);
+
+// else 
+//    current = -1 * num;
+
+// while(current < num-1)
+//    current++;
+//    Console.Write(current + " ");
+
+//5. Напишите программу, которая на вход принимает трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+// Console.Write("input N: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(num % 10);
+
