cda930a3d09a9eb2ef61a9ea7ac6ddc11062c918
Evgenia <olivia20011978@mail.ru>
2022-06-14 19:50:09 +0300

    homework is done

--- Program.cs
+++ Program.cs
@@ -55,3 +55,54 @@
 // int num = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine(num % 10);
 
+
+
+//homework
+//1.Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("input A: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input B: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num > num2)
+//     Console.WriteLine("max A");
+//  else
+//     Console.WriteLine("max B");
+
+//2.Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("input A: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input B: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input C: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int max = Convert.ToInt32(Console.ReadLine());
+// max = num; 
+// if (max < num2)
+//    max = num2;
+// if (max < num3)
+//    max = num3;
+// Console.Write("max = ");  // не могу понять как вывести значение max
+
+//3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.WriteLine("input num: ");
+// int num = Convert.ToInt32(Console.ReadLine());   
+// Console.WriteLine(num % 2);
+
+//4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("input N");
+int num = Convert.ToInt32(Console.ReadLine());
+int curr = Convert.ToInt32(Console.ReadLine());
+int current;
+if (num < 0)
+    current = num;
+    num = num * (-1);
+
+while(current < num-1)
+    Console.WriteLine(num % 2 == 0);
+    current++;
+    Console.Write(current + " ");  //логику решения поняла, а написать нормально не получается, не пойму в чем дело тут
+
